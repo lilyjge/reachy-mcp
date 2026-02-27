de90e23a3a378e5918aa60e70827df4985b422b0
Karthik Nambiar <karthik.nambiar@uwaterloo.ca>
2026-02-27 14:21:14 -0500

    added list emotions to init.py

--- server/reachy/controller/__init__.py
+++ server/reachy/controller/__init__.py
@@ -1,3 +1,3 @@
 from .audio import speak
-from .controls import _go_to, _get_running_moves, _go_to_default, _stop_all_moves, _play_emotion, move_to_audio, center_to_face
+from .controls import _go_to, _get_running_moves, _go_to_default, _stop_all_moves, _play_emotion, move_to_audio, center_to_face, _get_available_emotions
 from .vision import take_picture, save_image_person, describe_image, detect_faces, analyze_face, _IMAGES_DIR
